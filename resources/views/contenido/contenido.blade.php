628a3ea92f31ddc92b4398ec4501b29d2c3cfc8c
Dave <dave.jah94@gmail.com>
2020-04-20 19:45:36 -0600

    Añadí componente de ejemplo
    Está en el Menú Almacen/Artículos

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -9,7 +9,7 @@
             </template>
             
             <template v-if="menu==2">
-                <h1>Contenido del menú 2</h1>
+                <propuesta2></propuesta2>
             </template>
 
             <template v-if="menu==3">
